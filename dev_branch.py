4109cef62aedc7d55f3ad2e614b31279e756e475
Hunny77Kumar <h85947511@gmail.com>
2024-02-09 16:54:07 +0530

    dev branch first commit

---
+++ dev_branch.py
@@ -0,0 +1,9 @@
+class Person():
+    def __init__(self, firstname, secondname):
+        self.firstname = firstname
+        self.secondname = secondname
+
+    def display_info(self):
+        print("Name is : ", self.firstname,"second name is", self.secondname)
+
+    
