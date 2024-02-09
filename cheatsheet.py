ebca3b06904d55f9f781767143a4e06bf961962b
Hunny77Kumar <h85947511@gmail.com>
2024-02-09 14:50:58 +0530

    Git command

---
+++ cheatsheet.py
@@ -0,0 +1,22 @@
+(base) C:\DataScience_python\Hunny\GitPractise>git add .
+
+(base) C:\DataScience_python\Hunny\GitPractise>git status
+On branch main
+Changes to be committed:
+  (use "git restore --staged <file>..." to unstage)
+        modified:   app.py
+
+
+(base) C:\DataScience_python\Hunny\GitPractise>git commit -m "Third commit to bring data in app.py" 
+[main 3dfe497] Third commit to bring data in app.py
+ 1 file changed, 3 insertions(+)
+
+(base) C:\DataScience_python\Hunny\GitPractise>git push origin main
+Enumerating objects: 5, done.
+Counting objects: 100% (5/5), done.
+Delta compression using up to 8 threads
+Compressing objects: 100% (3/3), done.
+Writing objects: 100% (3/3), 340 bytes | 340.00 KiB/s, done.
+Total 3 (delta 0), reused 0 (delta 0), pack-reused 0
+To https://github.com/Hunny77Kumar/GitPractise.git
+   d7b46dc..3dfe497  main -> main
